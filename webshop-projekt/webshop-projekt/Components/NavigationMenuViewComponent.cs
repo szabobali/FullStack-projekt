efbfa338f7a05427bfa56f8df071aa2356d8af82
Szabó Balázs <sz.balazs988@gmail.com>
2025-05-16 09:56:09 +0200

    navbar is working

---
+++ webshop-projekt/webshop-projekt/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace webshop_projekt.Components
+{
+    public class NavigationMenuViewComponent
+    {
+        public String Invoke()
+        { return "Hello from the Nav View Component!"; }
+    }
+}
